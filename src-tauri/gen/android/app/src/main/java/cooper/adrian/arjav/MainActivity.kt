1995230075622a5a2697af2aeb753c7e69130506
Arjav Jain <arjavjain0703@gmail.com>
2025-08-09 07:19:28 -0400

    add android files

---
+++ src-tauri/gen/android/app/src/main/java/cooper/adrian/arjav/MainActivity.kt
@@ -0,0 +1,11 @@
+package cooper.adrian.arjav
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
