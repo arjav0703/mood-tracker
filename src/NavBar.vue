a27be5808a4fa84fa3f7be4174a7651ab13b7057
Adrian Tennies <adrian@tennies.us>
2025-08-09 15:38:50 -0400

    Merging Cooper with adrian

---
+++ src/NavBar.vue
@@ -0,0 +1,32 @@
+<template>  <div class="navbar">
+    <button @click="selectTab('Home')">Home</button>
+    <button @click="selectTab('Graphs')">About</button>
+    <button @click="selectTab('contact')">Contact</button>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      currentTab: 'home',
+    };
+  },
+  methods: {
+    selectTab(tab) {
+      this.currentTab = tab;
+      this.$emit('tab-selected', tab);
+    },
+  },
+};
+</script>
+<style>
+.navbar {
+  position: fixed;
+  bottom: 4.75%;
+  width: 150%;
+  display: flex;
+  justify-content: space-around;
+  background-color: #fff;
+  border-top: 1px solid #ccc;
+}
+</style>
